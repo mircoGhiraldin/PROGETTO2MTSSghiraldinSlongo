f3a63f3e46c39cbd36214e1b184f050df3c11332
LukeSlongo <luca.slongo@studenti.unipd.it>
2025-05-06 00:40:19 +0200

    Closes #8 - Metodo implementato modificando il valore di CyclomaticComplexity a 14 nel checkstyle, creato file Main.java per rendere più modulare il codice

---
+++ roman-number/src/main/java/it/unipd/mtss/Main.java
@@ -0,0 +1,17 @@
+////////////////////////////////////////////////////////////////////
+// Luca Slongo 2111009
+// Mirco Ghiraldin 2102505
+////////////////////////////////////////////////////////////////////
+
+package it.unipd.mtss;
+
+import it.unipd.mtss.IntegerToRoman;
+import it.unipd.mtss.RomanPrinter;
+
+public class Main {
+    public static void main(String[] args) {
+    int numero = 1987;
+    String romano = IntegerToRoman.convert(numero);
+    System.out.println("Il numero " + numero + " in numeri romani è: " + romano);
+    }  
+}
--- roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
+++ roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -2,6 +2,7 @@
 // Luca Slongo 2111009
 // Mirco Ghiraldin 2102505
 ////////////////////////////////////////////////////////////////////
+
 package it.unipd.mtss;
 public class RomanPrinter {
     public static String print(int num){
