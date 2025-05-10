17e1f024bf9f0fcd4d7636110d4e3cb40832e509
mircoGhiraldin <mirco.ghiraldin@studenti.unipd.it>
2025-05-10 16:05:13 +0200

    Closes #15 - implementata una test suite generica in cui sono raggruppati tutti i tests

---
+++ roman-number/src/test/java/it/unipd/mtss/AllTest.java
@@ -0,0 +1,13 @@
+package it.unipd.mtss;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    IntegerToRomanTest.class,
+    RomanPrinterTest.class,
+    Main.class
+})
+public class AllTest{
+    
+}
