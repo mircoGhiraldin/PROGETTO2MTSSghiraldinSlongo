f31aaebf348774515b648f7bc7b62c76adc10bac
mircoGhiraldin <mirco.ghiraldin@studenti.unipd.it>
2025-05-10 15:44:24 +0200

    Closes #12 - migliorati i test per fare in modo che rispettino la struttura AAA

--- roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -1,24 +1,116 @@
 package it.unipd.mtss;
 
-import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 public class IntegerToRomanTest {
-    @Test  
-    public void testConvert() {
-        assertEquals("I", IntegerToRoman.convert(1));
-        assertEquals("II", IntegerToRoman.convert(2));
-        assertEquals("IX", IntegerToRoman.convert(9));
-        assertEquals("XI", IntegerToRoman.convert(11));
-        assertEquals("XXV", IntegerToRoman.convert(25));
-        assertEquals("XXXIX", IntegerToRoman.convert(39));
-        assertEquals("CXXIII", IntegerToRoman.convert(123));
-        assertEquals("CCLV", IntegerToRoman.convert(255));
-        assertEquals("CDLVIII", IntegerToRoman.convert(458));
-        assertEquals("DCLXXXIII", IntegerToRoman.convert(683));
-        assertEquals("DCCCXV", IntegerToRoman.convert(815));
-        assertEquals("CMXCIX", IntegerToRoman.convert(999));
-        
-    }
 
+    @Test
+    public void testConvert() {
+   
+        int number = 1;
+        String expectedRoman = "I";
+        
+        String actualRoman = IntegerToRoman.convert(number);
+        
+        assertEquals(expectedRoman, actualRoman);
+        
+        
+        number = 2;
+        expectedRoman = "II";
+        
+        
+        actualRoman = IntegerToRoman.convert(number);
+        
+        assertEquals(expectedRoman, actualRoman);
+        
+        number = 9;
+        expectedRoman = "IX";
+        
+        
+        actualRoman = IntegerToRoman.convert(number);
+        
+        assertEquals(expectedRoman, actualRoman);
+        
+        number = 11;
+        expectedRoman = "XI";
+        
+        actualRoman = IntegerToRoman.convert(number);
+        
+        
+        assertEquals(expectedRoman, actualRoman);
+        
+        number = 25;
+        expectedRoman = "XXV";
+        
+        actualRoman = IntegerToRoman.convert(number);
+        
+
+        assertEquals(expectedRoman, actualRoman);
+        
+        number = 39;
+        expectedRoman = "XXXIX";
+        
+
+        actualRoman = IntegerToRoman.convert(number);
+        
+
+        assertEquals(expectedRoman, actualRoman);
+        
+
+        number = 123;
+        expectedRoman = "CXXIII";
+        
+
+        actualRoman = IntegerToRoman.convert(number);
+        
+
+        assertEquals(expectedRoman, actualRoman);
+        
+
+        number = 255;
+        expectedRoman = "CCLV";
+        
+
+        actualRoman = IntegerToRoman.convert(number);
+        
+
+        assertEquals(expectedRoman, actualRoman);
+        
+        number = 458;
+        expectedRoman = "CDLVIII";
+        
+        actualRoman = IntegerToRoman.convert(number);
+        
+        assertEquals(expectedRoman, actualRoman);
+        
+
+        number = 683;
+        expectedRoman = "DCLXXXIII";
+        
+        actualRoman = IntegerToRoman.convert(number);
+        
+        assertEquals(expectedRoman, actualRoman);
+        
+   
+        number = 815;
+        expectedRoman = "DCCCXV";
+        
+        actualRoman = IntegerToRoman.convert(number);
+        
+     
+        assertEquals(expectedRoman, actualRoman);
+        
+  
+        number = 999;
+        expectedRoman = "CMXCIX";
+        
+    
+        actualRoman = IntegerToRoman.convert(number);
+        
+   
+        assertEquals(expectedRoman, actualRoman);
+    }
 }
+
+
--- roman-number/src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ roman-number/src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -6,16 +6,63 @@ import static org.junit.Assert.assertEquals;
 public class RomanPrinterTest {
     @Test
     public void testPrint() {
-        assertEquals("  _____ \n |_   _|\n   | |  \n   | |  \n  _| |_ \n |_____|\n", RomanPrinter.print(1));
-        assertEquals("  _____   _____ \n |_   _| |_   _|\n   | |     | |  \n   | |     | |  \n  _| |_   _| |_ \n |_____| |_____|\n", RomanPrinter.print(2));
-        assertEquals(" __      __\n \\ \\    / /\n  \\ \\  / / \n   \\ \\/ /  \n    \\  /   \n     \\/    \n", RomanPrinter.print(5));
-        assertEquals("  _____  __   __\n |_   _| \\ \\ / /\n   | |    \\ V / \n   | |     > <  \n  _| |_   / . \\ \n |_____| /_/ \\_\\\n", RomanPrinter.print(9));
-        assertEquals(" __   __  _____  __   __\n \\ \\ / / |_   _| \\ \\ / /\n  \\ V /    | |    \\ V / \n   > <     | |     > <  \n  / . \\   _| |_   / . \\ \n /_/ \\_\\ |_____| /_/ \\_\\\n", RomanPrinter.print(19));
-        assertEquals(" __   __  _      \n \\ \\ / / | |     \n  \\ V /  | |     \n   > <   | |     \n  / . \\  | |____ \n /_/ \\_\\ |______|\n", RomanPrinter.print(40));
-        assertEquals("  _       __   __ __   __ __   __\n | |      \\ \\ / / \\ \\ / / \\ \\ / /\n | |       \\ V /   \\ V /   \\ V / \n | |        > <     > <     > <  \n | |____   / . \\   / . \\   / . \\ \n |______| /_/ \\_\\ /_/ \\_\\ /_/ \\_\\\n", RomanPrinter.print(80));
-        assertEquals("   _____    _____    _____ \n  / ____|  / ____|  / ____|\n | |      | |      | |     \n | |      | |      | |     \n | |____  | |____  | |____ \n  \\_____|  \\_____|  \\_____|\n", RomanPrinter.print(300));
-        assertEquals("   _____   _____  \n  / ____| |  __ \\ \n | |      | |  | |\n | |      | |  | |\n | |____  | |__| |\n  \\_____| |_____/ \n", RomanPrinter.print(400));
-        assertEquals("   _____   __  __ \n  / ____| |  \\/  |\n | |      | \\  / |\n | |      | |\\/| |\n | |____  | |  | |\n  \\_____| |_|  |_|\n", RomanPrinter.print(900));
-        assertEquals("  __  __ \n |  \\/  |\n | \\  / |\n | |\\/| |\n | |  | |\n |_|  |_|\n", RomanPrinter.print(1000));
+        int number = 1;
+        String  expected ="  _____ \n |_   _|\n   | |  \n   | |  \n  _| |_ \n |_____|\n";
+        String actual = RomanPrinter.print(number);
+        assertEquals(expected, actual);
+
+         number = 2;
+         expected ="  _____   _____ \n |_   _| |_   _|\n   | |     | |  \n   | |     | |  \n  _| |_   _| |_ \n |_____| |_____|\n";
+         actual = RomanPrinter.print(number);
+        assertEquals(expected, actual);
+
+         number = 5;
+         expected =" __      __\n \\ \\    / /\n  \\ \\  / / \n   \\ \\/ /  \n    \\  /   \n     \\/    \n";
+         actual = RomanPrinter.print(number);
+        assertEquals(expected, actual);
+
+         number = 9;
+         expected ="  _____  __   __\n |_   _| \\ \\ / /\n   | |    \\ V / \n   | |     > <  \n  _| |_   / . \\ \n |_____| /_/ \\_\\\n";
+         actual = RomanPrinter.print(number);
+        assertEquals(expected, actual);
+
+         number = 19;
+         expected =" __   __  _____  __   __\n \\ \\ / / |_   _| \\ \\ / /\n  \\ V /    | |    \\ V / \n   > <     | |     > <  \n  / . \\   _| |_   / . \\ \n /_/ \\_\\ |_____| /_/ \\_\\\n";
+         actual = RomanPrinter.print(number);
+        assertEquals(expected, actual);
+
+         number = 40;
+         expected =" __   __  _      \n \\ \\ / / | |     \n  \\ V /  | |     \n   > <   | |     \n  / . \\  | |____ \n /_/ \\_\\ |______|\n";
+         actual = RomanPrinter.print(number);
+        assertEquals(expected, actual);
+
+         number = 80;
+         expected ="  _       __   __ __   __ __   __\n | |      \\ \\ / / \\ \\ / / \\ \\ / /\n | |       \\ V /   \\ V /   \\ V / \n | |        > <     > <     > <  \n | |____   / . \\   / . \\   / . \\ \n |______| /_/ \\_\\ /_/ \\_\\ /_/ \\_\\\n";
+         actual = RomanPrinter.print(number);
+        assertEquals(expected, actual);
+
+         number = 300;
+         expected ="   _____    _____    _____ \n  / ____|  / ____|  / ____|\n | |      | |      | |     \n | |      | |      | |     \n | |____  | |____  | |____ \n  \\_____|  \\_____|  \\_____|\n";
+         actual = RomanPrinter.print(number);
+        assertEquals(expected, actual);
+
+         number = 400;
+         expected ="   _____   _____  \n  / ____| |  __ \\ \n | |      | |  | |\n | |      | |  | |\n | |____  | |__| |\n  \\_____| |_____/ \n";
+         actual = RomanPrinter.print(number);
+        assertEquals(expected, actual);
+
+         number = 900;
+         expected ="   _____   __  __ \n  / ____| |  \\/  |\n | |      | \\  / |\n | |      | |\\/| |\n | |____  | |  | |\n  \\_____| |_|  |_|\n";
+         actual = RomanPrinter.print(number);
+        assertEquals(expected, actual);
+
+        number = 1000;
+         expected ="  __  __ \n |  \\/  |\n | \\  / |\n | |\\/| |\n | |  | |\n |_|  |_|\n";
+         actual = RomanPrinter.print(number);
+        assertEquals(expected, actual);
+
+
+        
+        
     }
 }
